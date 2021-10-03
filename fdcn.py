710709dd751f74d4f7495582cf6b1550e814e61f
Jean Gabès <naparuba@gmail.com>
2021-10-03 15:13:27 +0200

    Add: Affiche les success et les bonnes/mauvaises fins

--- fdcn.py
+++ fdcn.py
@@ -2,6 +2,7 @@
 
 import graphviz
 import json
+import sys
 
 display_graph = graphviz.Digraph('G', filename='graph/fdcn', format='png')
 
@@ -56,7 +57,7 @@ class Node(object):
         self._id = _id
         
         self._ending = None
-        self._from_node_ending = None
+        self._success = None
         
         self._sons = []
         
@@ -77,6 +78,25 @@ class Node(object):
         self._ending = ending
     
     
+    def set_sucess(self, success):
+        self._success = success
+    
+    
+    def _get_ending_color(self):
+        return {ENDINGS.GOOD: 'darkseagreen1', ENDINGS.BAD: 'crimson'}.get(self._ending)  # we are sure it's one of this value
+    
+    
+    def _get_border_color(self):
+        if self._success is not None:
+            return 'gold'
+        return 'black'
+    
+    def _get_penwidth(self):
+        if self._success is not None:
+            return '3.0'
+        return '1.0'
+    
+    
     def add_son(self, son):
         self._sons.append(son)
     
@@ -84,20 +104,23 @@ class Node(object):
     def add_node_to_display_graph(self, display_graph):
         graph = display_graph
         
+        border_color= self._get_border_color()
+        penwidth = self._get_penwidth()
+        
         if self._is_chapter_start:
             node_id_string = '%s' % self._id
-            graph.node(node_id_string, shape='doubleoctagon', style='filled', color='lightgrey', fillcolor='green', label='%s' % (node_id_string))
+            graph.node(node_id_string, shape='doubleoctagon', style='filled', color=border_color, penwidth=penwidth, fillcolor='green', label='%s' % (node_id_string))
         elif self._ending is not None:
             # First myself
             node_id_string = '%s' % self._id
-            graph.node(node_id_string, shape='ellipse', style='solid', fillcolor='white', label='%s' % (node_id_string))
+            graph.node(node_id_string, shape='ellipse', style='solid', color=border_color, penwidth=penwidth, fillcolor='white', label='%s' % (node_id_string))
             
             # And also add the visual ending node
             node_id_string = "end-from-%s" % self._id
-            graph.node(node_id_string, shape='doubleoctagon', style='filled', color='lightgrey', fillcolor='yellow', label=r'End-from-%s' % self._id)
+            graph.node(node_id_string, shape='doubleoctagon', style='filled', color=border_color, penwidth=penwidth, fillcolor=self._get_ending_color(), label='End (%s)' % self._id)
         else:  # classic node
             node_id_string = '%s' % self._id
-            graph.node(node_id_string, label='%s' % (node_id_string), shape='ellipse', style='solid', fillcolor='white')
+            graph.node(node_id_string, label='%s' % (node_id_string), color=border_color, penwidth=penwidth, shape='ellipse', style='solid', fillcolor='white')
     
     
     def _get_graph_from_nodes(self, other, arc_graphs):
@@ -146,47 +169,30 @@ node_graph = Graph()
 for node_id_str in book_data.keys():
     node_graph.create_node(node_id_str)
 
-
-def create_node(idx, end_from_idx=''):
-    return
-    idx = '%s' % idx
-    # print('.. creating %s (%s)' % (idx, type(idx)))
-    # do not create node two times
-    if idx in node_created and idx != '608':
-        # print('ALREADY FOUNDED')
-        return
-    node_created.add(idx)
-    # print('NODE CREATED: %s (%s)' % (node_created, len(node_created)))
-    if int(idx) % 100 == 0:
-        print('100 aine %s' % type(idx))
-        display_graph.attr('node', shape='doubleoctagon', style='filled', color='lightgrey', fillcolor='green')
-        display_graph.node('%s' % idx)
-        print('NEW NODE (100): %s' % idx)
-    elif int(idx) == 608:
-        n_id = "fin-%s" % end_from_idx
-        display_graph.attr('node', shape='doubleoctagon', style='filled', color='lightgrey', fillcolor='yellow')
-        display_graph.node(n_id)
-        print('NEW NODE (END): %s' % n_id)
-    else:
-        display_graph.attr('node', shape='ellipse', style='solid', fillcolor='white')
-        display_graph.node('%s' % idx)
-        print('NEW NODE      : %s' % idx)
-
-
 for idx, n in book_data.items():
     idx = int(idx)
-    # print('Node: %s' % idx)
-    
-    create_node(idx)
     
     node = node_graph.get_node(idx)
+
+    success = n.get('success', None)
+    if success:
+        node.set_sucess(success)
     
     goto = n['goto']
     
     gotos = []
     if isinstance(goto, int):
         if goto == 608:
-            node.set_ending(ENDINGS.GOOD)
+            ending = n.get('ending', None)
+            if ending is None:
+                print('ERROR: node %s is an end without ending' % idx)
+                sys.exit(2)
+            _ending = {'good': ENDINGS.GOOD, 'bad': ENDINGS.BAD}.get(ending, None)
+            if _ending is None:
+                print('ERROR: node %s have an unknown ending string: %s' % (idx, ending))
+                sys.exit(2)
+            
+            node.set_ending(_ending)
         else:
             son = node_graph.get_node(goto)
             node.add_son(son)
