1ff67a875cd4cef7deec3dedc88ea3269ddd696f
jean.gabes <jean.gabes@contentsquare.com>
2026-07-09 12:38:29 +0200

    Convert: remplacement GUT 7.2.0 -> 9.7.0 (compatibilité Godot 4.7)
    Remplacement complet du dossier addons/gut/ (aucun patch local à
    préserver, confirmé en amont par git log). Plugin activé explicitement
    dans project.godot ([editor_plugins] enabled=...).
    
    Test canari (test/unit/test_player.gd, le plus simple du projet -- pas de
    yield, pas de .instance(), pas d'accès fichier) : 2/2 tests passent sous
    Godot 4.7 + GUT 9.7.0. Valide toute la chaîne d'outillage (moteur +
    framework de test + autoloads) avant d'attaquer la correction des ~34
    fichiers de test restants.

--- addons/gut/gui/gut_config_gui.gd
+++ addons/gut/gui/gut_config_gui.gd
@@ -1,97 +1,41 @@
-var _base_container = null
-var _base_control = null
+var PanelControls = load("res://addons/gut/gui/panel_controls.gd")
+var GutConfig = load('res://addons/gut/gut_config.gd')
+
 const DIRS_TO_LIST = 6
+
+# specific titles that we need to do stuff with
+var _titles = {
+	dirs = null
+}
+
 var _cfg_ctrls = {}
-var _avail_fonts = ['AnonymousPro', 'CourierPrime', 'LobsterTwo', 'Default']
-
-
-signal changed
+var opt_maker = null
 
 func _init(cont):
-	_base_container = cont
+	opt_maker = GutUtils.OptionMaker.new(cont)
+	_cfg_ctrls = opt_maker.controls
+	# _base_container = cont
 
-	_base_control = HBoxContainer.new()
-	_base_control.size_flags_horizontal = _base_control.SIZE_EXPAND_FILL
-	_base_control.mouse_filter = _base_control.MOUSE_FILTER_PASS
-	
-	var lbl = Label.new()
-	lbl.size_flags_horizontal = lbl.SIZE_EXPAND_FILL
-	lbl.mouse_filter = lbl.MOUSE_FILTER_STOP
-	_base_control.add_child(lbl)
 
-# ------------------
-# Private
-# ------------------
-func _new_row(key, disp_text, value_ctrl, hint):
-	var ctrl = _base_control.duplicate()
-	var lbl = ctrl.get_node("Label")
-	
-	lbl.tooltip_text = hint
-	lbl.text = disp_text
-	_base_container.add_child(ctrl)
-	
-	_cfg_ctrls[key] = value_ctrl
-	ctrl.add_child(value_ctrl)
+func _add_save_load():
+	var ctrl = PanelControls.SaveLoadControl.new('Config', '', '')
 
+	ctrl.save_path_chosen.connect(_on_save_path_chosen)
+	ctrl.load_path_chosen.connect(_on_load_path_chosen)
+
+	#_cfg_ctrls['save_load'] = ctrl
+	opt_maker.add_ctrl('save_load', ctrl)
 	return ctrl
-
-
-func _add_title(text):
-	var row = _base_control.duplicate()
-	var lbl = row.get_node('Label')
-	
-	lbl.text = text
-	lbl.align = Label.ALIGNMENT_CENTER
-	_base_container.add_child(row)
-	
-	row.connect('draw', Callable(self, '_on_title_cell_draw').bind(row))
-
-
-func _add_number(key, value, disp_text, v_min, v_max, hint=''):
-	var value_ctrl = SpinBox.new()
-	value_ctrl.value = value
-	value_ctrl.min_value = v_min
-	value_ctrl.max_value = v_max
-	
-	var ctrl = _new_row(key, disp_text, value_ctrl, hint)
-
-
-func _add_select(key, value, values, disp_text, hint=''):
-	var value_ctrl = OptionButton.new()
-	var select_idx = 0
-	for i in range(values.size()):
-		value_ctrl.add_item(values[i])
-		if(value == values[i]):
-			select_idx = i
-	value_ctrl.selected = select_idx
-	
-	var ctrl = _new_row(key, disp_text, value_ctrl, hint)
-
-
-func _add_value(key, value, disp_text, hint=''):
-	var value_ctrl = LineEdit.new()
-	value_ctrl.size_flags_horizontal = value_ctrl.SIZE_EXPAND_FILL
-	value_ctrl.text = value
-	
-	var ctrl = _new_row(key, disp_text, value_ctrl, hint)
-
-
-func _add_boolean(key, value, disp_text, hint=''):
-	var value_ctrl = CheckBox.new()
-	value_ctrl.button_pressed = value
-	
-	var ctrl = _new_row(key, disp_text, value_ctrl, hint)
-
 
 # ------------------
 # Events
 # ------------------
-func _on_ctrl_value_changed(which):
-	pass
-	
-	
-func _on_title_cell_draw(which):
-	which.draw_rect(Rect2(Vector2(0, 0), which.size), Color(0, 0, 0, .15))
+func _on_save_path_chosen(path):
+	save_file(path)
+
+
+func _on_load_path_chosen(path):
+	load_file.bind(path).call_deferred()
 
 # ------------------
 # Public
@@ -99,95 +43,240 @@ func _on_title_cell_draw(which):
 func get_config_issues():
 	var to_return = []
 	var has_directory = false
-	var dir = DirAccess.new()
-	
+
 	for i in range(DIRS_TO_LIST):
 		var key = str('directory_', i)
-		var path = _cfg_ctrls[key].text
+		var path = _cfg_ctrls[key].value
 		if(path != null and path != ''):
 			has_directory = true
-			if(!dir.dir_exists(path)):
+			if(!DirAccess.dir_exists_absolute(path)):
+				_cfg_ctrls[key].mark_invalid(true)
 				to_return.append(str('Test directory ', path, ' does not exist.'))
-				
+			else:
+				_cfg_ctrls[key].mark_invalid(false)
+		else:
+			_cfg_ctrls[key].mark_invalid(false)
+
 	if(!has_directory):
 		to_return.append('You do not have any directories set.')
-				
-	if(_cfg_ctrls['prefix'].text == ''):
-		to_return.append("You must set a Script prefix or GUT won't find any scripts")
-		
+		_titles.dirs.mark_invalid(true)
+	else:
+		_titles.dirs.mark_invalid(false)
+
+	if(!_cfg_ctrls.suffix.value.ends_with('.gd')):
+		_cfg_ctrls.suffix.mark_invalid(true)
+		to_return.append("Script suffix must end in '.gd'")
+	else:
+		_cfg_ctrls.suffix.mark_invalid(false)
+
 	return to_return
 
 
-func set_options(options):
-	_add_title("Settings")
-	_add_number("log_level", options.log_level, "Log Level", 0, 3,
-		"Detail level for log messages.")
-	_add_boolean('ignore_pause', options.ignore_pause, 'Ignore Pause', 
+func clear():
+	opt_maker.clear()
+
+
+func save_file(path):
+	var gcfg = GutConfig.new()
+	gcfg.options = get_options({})
+	gcfg.save_file(path)
+
+
+
+func load_file(path):
+	var gcfg = GutConfig.new()
+	gcfg.load_options(path)
+	clear()
+	set_options(gcfg.options)
+
+
+# --------------
+# SUPER dumb but VERY fun hack to hide settings.  The various _add methods will
+# return what they add.  If you want to hide it, just assign the result to this.
+# YES, I could have just put .visible at the end, but I didn't think of that
+# until just now, and this was fun, non-permanent and the .visible at the end
+# isn't as obvious as hide_this =
+#
+# Also, we can't just skip adding the controls because other things are looking
+# for them and things start to blow up if you don't add them.
+var hide_this = null :
+	set(val):
+		val.visible = false
+
+# --------------
+
+func set_options(opts):
+	var options = opts.duplicate()
+
+	# _add_title('Save/Load')
+	_add_save_load()
+
+	opt_maker.add_title("Settings")
+	opt_maker.add_number("log_level", options.log_level, "Log Level", 0, 3,
+		"Detail level for log messages.\n" + \
+		"\t0: Errors and failures only.\n" + \
+		"\t1: Adds all test names + warnings + info\n" + \
+		"\t2: Shows all asserts\n" + \
+		"\t3: Adds more stuff probably, maybe not.")
+	opt_maker.add_float("wait_log_delay", options.wait_log_delay, "Wait Log Delay", 0.1, 0.0, 999.1,
+		"How long to wait before displaying 'Awaiting' messages.")
+	opt_maker.add_boolean('ignore_pause', options.ignore_pause, 'Ignore Pause',
 		"Ignore calls to pause_before_teardown")
-	_add_boolean('should_exit', options.should_exit, 'Exit on Finish',
+	opt_maker.add_boolean('hide_orphans', options.hide_orphans, 'Hide Orphans',
+		'Do not display orphan counts in output.')
+	opt_maker.add_boolean('should_exit', options.should_exit, 'Exit on Finish',
 		"Exit when tests finished.")
-	_add_boolean('should_exit_on_success', options.should_exit_on_success, 'Exit on Success',
-		"Exit if there are no failures.  Does nothing if 'Exit on Finish' is set.")
-	_add_boolean('should_maximize', options.should_maximize, 'Maximize',
-		"Maximize GUT when tests are being run.")
-	_add_number('opacity', options.opacity, 'Opacity', 0, 100,
+	opt_maker.add_boolean('should_exit_on_success', options.should_exit_on_success, 'Exit on Success',
+		"Exit if there are no failures.  Does nothing if 'Exit on Finish' is enabled.")
+	opt_maker.add_select('double_strategy', 'Script Only', ['Include Native', 'Script Only'], 'Double Strategy',
+		'"Include Native" will include native methods in Doubles.  "Script Only" will not.  ' + "\n" + \
+		'The native method override warning is disabled when creating Doubles.' + "\n" + \
+		'This is the default, you can override this at the script level or when creating doubles.')
+	_cfg_ctrls.double_strategy.value = GutUtils.get_enum_value(
+		options.double_strategy, GutUtils.DOUBLE_STRATEGY, GutUtils.DOUBLE_STRATEGY.SCRIPT_ONLY)
+
+
+	opt_maker.add_title("Fail Error Types")
+	opt_maker.add_boolean("error_tracking", !options.no_error_tracking, 'Track Errors',
+		"Enable/Disable GUT's ability to detect engine and push errors.")
+	opt_maker.add_boolean('engine_errors_cause_failure', options.failure_error_types.has(GutConfig.FAIL_ERROR_TYPE_ENGINE),
+		'Engine', 'Any script/engine error that occurs during a test will cause the test to fail.')
+	opt_maker.add_boolean('push_error_errors_cause_failure', options.failure_error_types.has(GutConfig.FAIL_ERROR_TYPE_PUSH_ERROR),
+		'Push', 'Any error generated by a call to push_error that occurs during a test will cause the test to fail.')
+	opt_maker.add_boolean('gut_errors_cause_failure', options.failure_error_types.has(GutConfig.FAIL_ERROR_TYPE_GUT),
+		'GUT', 'Any internal GUT error that occurs while a test is running will cause it to fail..')
+
+
+	opt_maker.add_title('Runner Appearance')
+	hide_this = opt_maker.add_boolean("gut_on_top", options.gut_on_top, "On Top",
+		"The GUT Runner appears above children added during tests.")
+	opt_maker.add_number('opacity', options.opacity, 'Opacity', 0, 100,
 		"The opacity of GUT when tests are running.")
+	hide_this = opt_maker.add_boolean('should_maximize', options.should_maximize, 'Maximize',
+		"Maximize GUT when tests are being run.")
+	opt_maker.add_boolean('compact_mode', options.compact_mode, 'Compact Mode',
+		'The runner will be in compact mode.  This overrides Maximize.')
+	opt_maker.add_select('font_name', options.font_name, GutUtils.avail_fonts, 'Font',
+		"The font to use for text output in the Gut Runner.")
+	opt_maker.add_number('font_size', options.font_size, 'Font Size', 5, 100,
+		"The font size for text output in the Gut Runner.")
+	hide_this = opt_maker.add_color('font_color', options.font_color, 'Font Color',
+		"The font color for text output in the Gut Runner.")
+	opt_maker.add_color('background_color', options.background_color, 'Background Color',
+		"The background color for text output in the Gut Runner.")
+	opt_maker.add_boolean('disable_colors', options.disable_colors, 'Disable Formatting',
+		'Disable formatting and colors used in the Runner.  Does not affect panel output.')
 
-	_add_title("XML Output")
-	_add_value("junit_xml_file", options.junit_xml_file, "Output Path3D",
-		"Path3D and filename where GUT should create the JUnit XML file.")
-	_add_boolean("junit_xml_timestamp", options.junit_xml_timestamp, "Include timestamp",
-		"Include a timestamp in the filename so that each run gets its own xml file.")
 
-	_add_title('Font')
-	_add_select('font_name', options.font_name, _avail_fonts, 'Font',
-		"The name of the font to use when running tests and in the output panel to the left.")
-	_add_number('font_size', options.font_size, 'Font Size', 5, 100,
-		"The font size to use when running tests and in the output panel to the left.")
-
-	_add_title('Directories')
-	_add_boolean('include_subdirs', options.include_subdirs, 'Include Subdirs', 
+	_titles.dirs = opt_maker.add_title('Test Directories')
+	opt_maker.add_boolean('include_subdirs', options.include_subdirs, 'Include Subdirs',
 		"Include subdirectories of the directories configured below.")
+
+	var dirs_to_load = options.configured_dirs
+	if(options.dirs.size() > dirs_to_load.size()):
+		dirs_to_load = options.dirs
+
 	for i in range(DIRS_TO_LIST):
 		var value = ''
-		if(options.dirs.size() > i):
-			value = options.dirs[i]
+		if(dirs_to_load.size() > i):
+			value = dirs_to_load[i]
 
-		_add_value(str('directory_', i), value, str('DirAccess ', i))
+		var test_dir = opt_maker.add_directory(str('directory_', i), value, str(i))
+		test_dir.enabled_button.visible = true
+		test_dir.enabled_button.button_pressed = options.dirs.has(value)
 
-	_add_title('Misc')
-	_add_value('prefix', options.prefix, 'Script Prefix',
+
+	opt_maker.add_title("XML Output")
+	opt_maker.add_save_file_anywhere("junit_xml_file", options.junit_xml_file, "Output Path",
+		"Path and filename where GUT should create a JUnit compliant XML file.  " +
+		"This file will contain the results of the last test run.  To avoid " +
+		"overriding the file use Include Timestamp.")
+	opt_maker.add_boolean("junit_xml_timestamp", options.junit_xml_timestamp, "Include Timestamp",
+		"Include a timestamp in the filename so that each run gets its own xml file.")
+
+
+	opt_maker.add_title('Hooks')
+	opt_maker.add_file('pre_run_script', options.pre_run_script, 'Pre-Run Hook',
+		'This script will be run by GUT before any tests are run.')
+	opt_maker.add_file('post_run_script', options.post_run_script, 'Post-Run Hook',
+		'This script will be run by GUT after all tests are run.')
+
+
+	opt_maker.add_title('Misc')
+	opt_maker.add_value('prefix', options.prefix, 'Script Prefix',
 		"The filename prefix for all test scripts.")
+	opt_maker.add_value('suffix', options.suffix, 'Script Suffix',
+		"Script suffix, including .gd extension.  For example '_foo.gd'.")
+	opt_maker.add_float('paint_after', options.paint_after, 'Paint After', .05, 0.0, 1.0,
+		"How long GUT will wait before pausing for 1 frame to paint the screen.  0 is never.")
 
 
 
 func get_options(base_opts):
 	var to_return = base_opts.duplicate()
 
+	# Settings
 	to_return.log_level = _cfg_ctrls.log_level.value
-	to_return.ignore_pause = _cfg_ctrls.ignore_pause.pressed
-	to_return.should_exit = _cfg_ctrls.should_exit.pressed
-	to_return.should_exit_on_success = _cfg_ctrls.should_exit_on_success.pressed
-	to_return.should_maximize = _cfg_ctrls.should_maximize.pressed
-	to_return.opacity = _cfg_ctrls.opacity.value
+	to_return.wait_log_delay = _cfg_ctrls.wait_log_delay.value
+	to_return.ignore_pause = _cfg_ctrls.ignore_pause.value
+	to_return.hide_orphans = _cfg_ctrls.hide_orphans.value
+	to_return.should_exit = _cfg_ctrls.should_exit.value
+	to_return.should_exit_on_success = _cfg_ctrls.should_exit_on_success.value
+	to_return.double_strategy = _cfg_ctrls.double_strategy.value
 
-	to_return.junit_xml_file = _cfg_ctrls.junit_xml_file.text
-	to_return.junit_xml_timestamp = _cfg_ctrls.junit_xml_timestamp.pressed
-
-	to_return.font_name = _cfg_ctrls.font_name.get_item_text(
-		_cfg_ctrls.font_name.selected)
+	# Runner Appearance
+	to_return.font_name = _cfg_ctrls.font_name.text
 	to_return.font_size = _cfg_ctrls.font_size.value
+	to_return.should_maximize = _cfg_ctrls.should_maximize.value
+	to_return.compact_mode = _cfg_ctrls.compact_mode.value
+	to_return.opacity = _cfg_ctrls.opacity.value
+	to_return.background_color = _cfg_ctrls.background_color.value.to_html()
+	to_return.font_color = _cfg_ctrls.font_color.value.to_html()
+	to_return.disable_colors = _cfg_ctrls.disable_colors.value
+	to_return.gut_on_top = _cfg_ctrls.gut_on_top.value
+	to_return.paint_after = _cfg_ctrls.paint_after.value
 
-	to_return.include_subdirs = _cfg_ctrls.include_subdirs.pressed
+	# Fail Error Types
+	to_return.no_error_tracking = !_cfg_ctrls.error_tracking
+
+	var fail_error_types = []
+	if(_cfg_ctrls.engine_errors_cause_failure.value):
+		fail_error_types.append(GutConfig.FAIL_ERROR_TYPE_ENGINE)
+	if(_cfg_ctrls.push_error_errors_cause_failure.value):
+		fail_error_types.append(GutConfig.FAIL_ERROR_TYPE_PUSH_ERROR)
+	if(_cfg_ctrls.gut_errors_cause_failure.value):
+		fail_error_types.append(GutConfig.FAIL_ERROR_TYPE_GUT)
+	to_return.failure_error_types = fail_error_types
+
+	# Directories
+	to_return.include_subdirs = _cfg_ctrls.include_subdirs.value
 	var dirs = []
+	var configured_dirs = []
 	for i in range(DIRS_TO_LIST):
 		var key = str('directory_', i)
-		var val = _cfg_ctrls[key].text
-		if(val != '' and val != null):
-			dirs.append(val)
+		var ctrl = _cfg_ctrls[key]
+		if(ctrl.value != '' and ctrl.value != null):
+			configured_dirs.append(ctrl.value)
+			if(ctrl.enabled_button.button_pressed):
+				dirs.append(ctrl.value)
 	to_return.dirs = dirs
+	to_return.configured_dirs = configured_dirs
 
-	to_return.prefix = _cfg_ctrls.prefix.text
+	# XML Output
+	to_return.junit_xml_file = _cfg_ctrls.junit_xml_file.value
+	to_return.junit_xml_timestamp = _cfg_ctrls.junit_xml_timestamp.value
 
+	# Hooks
+	to_return.pre_run_script = _cfg_ctrls.pre_run_script.value
+	to_return.post_run_script = _cfg_ctrls.post_run_script.value
+
+	# Misc
+	to_return.prefix = _cfg_ctrls.prefix.value
+	to_return.suffix = _cfg_ctrls.suffix.value
 
 	return to_return
+
+
+func mark_saved():
+	for key in _cfg_ctrls:
+		_cfg_ctrls[key].mark_unsaved(false)
--- addons/gut/method_maker.gd
+++ addons/gut/method_maker.gd
@@ -1,10 +1,19 @@
 class CallParameters:
 	var p_name = null
 	var default = null
+	var vararg = false
 
-	func _init(n, d):
+	func _init(n,d):
 		p_name = n
 		default = d
+
+	func get_signature():
+		if(vararg):
+			return "...args: Array"
+		else:
+			return str(p_name, "=", default)
+
+
 
 
 # ------------------------------------------------------------------------------
@@ -25,138 +34,36 @@ class CallParameters:
 # 	(usage:7)
 # }]
 # default_args []
-
-var _utils = load('res://addons/gut/utils.gd').get_instance()
-var _lgr = _utils.get_logger()
 const PARAM_PREFIX = 'p_'
 
-# ------------------------------------------------------
-# _supported_defaults
-#
-# This array contains all the data types that are supported for default values.
-# If a value is supported it will contain either an empty string or a prefix
-# that should be used when setting the parameter default value.
-# For example int, real, bool do not need anything func(p1=1, p2=2.2, p3=false)
-# but things like Vectors and Colors do since only the parameters to create a
-# new Vector or Color are included in the metadata.
-# ------------------------------------------------------
-	# TYPE_NIL = 0 — Variable is of type nil (only applied for null).
-	# TYPE_BOOL = 1 — Variable is of type bool.
-	# TYPE_INT = 2 — Variable is of type int.
-	# TYPE_REAL = 3 — Variable is of type float/real.
-	# TYPE_STRING = 4 — Variable is of type String.
-	# TYPE_VECTOR2 = 5 — Variable is of type Vector2.
-	# TYPE_RECT2 = 6 — Variable is of type Rect2.
-	# TYPE_VECTOR3 = 7 — Variable is of type Vector3.
-	# TYPE_COLOR = 14 — Variable is of type Color.
-	# TYPE_OBJECT = 17 — Variable is of type Object.
-	# TYPE_DICTIONARY = 18 — Variable is of type Dictionary.
-	# TYPE_ARRAY = 19 — Variable is of type Array.
-	# TYPE_VECTOR2_ARRAY = 24 — Variable is of type PoolVector2Array.
 
+var _lgr = GutUtils.get_logger()
+static var _func_template = GutUtils.get_file_as_text('res://addons/gut/double_templates/function_template.txt')
+static var _init_template = GutUtils.get_file_as_text('res://addons/gut/double_templates/init_template.txt')
 
-
-# TYPE_TRANSFORM2D = 8 — Variable is of type Transform2D.
-# TYPE_PLANE = 9 — Variable is of type Plane.
-# TYPE_QUAT = 10 — Variable is of type Quat.
-# TYPE_AABB = 11 — Variable is of type AABB.
-# TYPE_BASIS = 12 — Variable is of type Basis.
-# TYPE_TRANSFORM = 13 — Variable is of type Transform.
-# TYPE_NODE_PATH = 15 — Variable is of type NodePath.
-# TYPE_RID = 16 — Variable is of type RID.
-# TYPE_RAW_ARRAY = 20 — Variable is of type PoolByteArray.
-# TYPE_INT_ARRAY = 21 — Variable is of type PoolIntArray.
-# TYPE_REAL_ARRAY = 22 — Variable is of type PoolRealArray.
-# TYPE_STRING_ARRAY = 23 — Variable is of type PoolStringArray.
-# TYPE_VECTOR3_ARRAY = 25 — Variable is of type PoolVector3Array.
-# TYPE_COLOR_ARRAY = 26 — Variable is of type PoolColorArray.
-# TYPE_MAX = 27 — Marker for end of type constants.
-# ------------------------------------------------------
-var _supported_defaults = []
-
-func _init():
-	for _i in range(TYPE_MAX):
-		_supported_defaults.append(null)
-
-	# These types do not require a prefix for defaults
-	_supported_defaults[TYPE_NIL] = ''
-	_supported_defaults[TYPE_BOOL] = ''
-	_supported_defaults[TYPE_INT] = ''
-	_supported_defaults[TYPE_FLOAT] = ''
-	_supported_defaults[TYPE_OBJECT] = ''
-	_supported_defaults[TYPE_ARRAY] = ''
-	_supported_defaults[TYPE_STRING] = ''
-	_supported_defaults[TYPE_DICTIONARY] = ''
-	_supported_defaults[TYPE_PACKED_VECTOR2_ARRAY] = ''
-
-	# These require a prefix for whatever default is provided
-	_supported_defaults[TYPE_VECTOR2] = 'Vector2'
-	_supported_defaults[TYPE_RECT2] = 'Rect2'
-	_supported_defaults[TYPE_VECTOR3] = 'Vector3'
-	_supported_defaults[TYPE_COLOR] = 'Color'
 
 # ###############
 # Private
 # ###############
-var _func_text = _utils.get_file_as_text('res://addons/gut/double_templates/function_template.txt')
-
-func _is_supported_default(type_flag):
-	return type_flag >= 0 and type_flag < _supported_defaults.size() and [type_flag] != null
-
 
 func _make_stub_default(method, index):
-	return str('__gut_default_val("', method, '",', index, ')')
+	return str('__gutdbl.default_val("', method, '",', index, ')')
 
-func _make_arg_array(method_meta, override_size):
+
+func _make_arg_array(method_meta):
 	var to_return = []
-
-	var has_unsupported_defaults = false
-	var dflt_start = method_meta.args.size() - method_meta.default_args.size()
 
 	for i in range(method_meta.args.size()):
 		var pname = method_meta.args[i].name
-		var dflt_text = ''
-
-		if(i < dflt_start):
-			dflt_text = _make_stub_default(method_meta.name, i)
-		else:
-			var dflt_idx = dflt_start - i
-			var t = method_meta.args[i]['type']
-			if(_is_supported_default(t)):
-				# strings are special, they need quotes around the value
-				if(t == TYPE_STRING):
-					dflt_text = str("'", str(method_meta.default_args[dflt_idx]), "'")
-				# Colors need the parens but things like Vector2 and Rect2 don't
-				elif(t == TYPE_COLOR):
-					dflt_text = str(_supported_defaults[t], '(', str(method_meta.default_args[dflt_idx]), ')')
-				elif(t == TYPE_OBJECT):
-					if(str(method_meta.default_args[dflt_idx]) == "[Object:null]"):
-						dflt_text = str(_supported_defaults[t], 'null')
-					else:
-						dflt_text = str(_supported_defaults[t], str(method_meta.default_args[dflt_idx]).to_lower())
-
-				# Everything else puts the prefix (if one is there) form _supported_defaults
-				# in front.  The to_lower is used b/c for some reason the defaults for
-				# null, true, false are all "Null", "True", "False".
-				else:
-					dflt_text = str(_supported_defaults[t], str(method_meta.default_args[dflt_idx]).to_lower())
-			else:
-				_lgr.error(str(
-					'Unsupported default param type:  ',method_meta.name, '-', method_meta.args[i].name, ' ', t, ' = ', method_meta.default_args[dflt_idx]))
-				dflt_text = str('unsupported=',t)
-				has_unsupported_defaults = true
-
-		# Finally add in the parameter
+		var dflt_text = _make_stub_default(method_meta.name, i)
 		to_return.append(CallParameters.new(PARAM_PREFIX + pname, dflt_text))
 
-	# Add in extra parameters from stub settings.
-	if(override_size != null):
-		for i in range(method_meta.args.size(), override_size):
-			var pname = str(PARAM_PREFIX, 'arg', i)
-			var dflt_text = _make_stub_default(method_meta.name, i)
-			to_return.append(CallParameters.new(pname, dflt_text))
+	if(method_meta.flags & METHOD_FLAG_VARARG):
+		var cp = CallParameters.new("args", "")
+		cp.vararg = true
+		to_return.append(cp)
 
-	return [has_unsupported_defaults, to_return];
+	return to_return
 
 
 # Creates a list of parameters with defaults of null unless a default value is
@@ -169,21 +76,36 @@ func _get_arg_text(arg_array):
 	var text = ''
 
 	for i in range(arg_array.size()):
-		text += str(arg_array[i].p_name, '=', arg_array[i].default)
+		text += arg_array[i].get_signature()
 		if(i != arg_array.size() -1):
 			text += ', '
 
 	return text
 
 
-func _get_super_call_text(method_name, args):
+# creates a call to the function in meta in the super's class.
+func _get_super_call_text(parsed_method, singleton):
+	var return_it = ''
+	if(parsed_method.return_type_text != 'void'):
+		return_it = 'return '
+
+	if parsed_method.meta.flags & MethodFlags.METHOD_FLAG_VIRTUAL_REQUIRED != 0:
+		if(return_it != ''):
+			return_it = '; return null'
+		return '__gutdbl.gut_ref.get_ref().get_logger().error(' + \
+			'"Cannot call super() because method %s is abstract.")%s' \
+			% [parsed_method.meta.name, return_it]
+
 	var params = ''
-	for i in range(args.size()):
-		params += args[i].p_name
-		if(i != args.size() -1):
+	for i in range(parsed_method.args.size()):
+		params += PARAM_PREFIX + parsed_method.args[i].name
+		if(i != parsed_method.args.size() -1):
 			params += ', '
 
-	return str('.', method_name, '(', params, ')')
+	if(singleton != null):
+		return str(return_it, 'await __gutdbl.get_singleton().', parsed_method.meta.name, '(', params, ')')
+	else:
+		return str(return_it, 'await super(', params, ')')
 
 
 func _get_spy_call_parameters_text(args):
@@ -199,42 +121,64 @@ func _get_spy_call_parameters_text(args):
 
 	return called_with
 
+
+func _get_init_text(meta, args, method_params, param_array):
+	var text = null
+
+	var decleration = str('func ', meta.name, '(', method_params, ')')
+	var super_params = ''
+	if(args.size() > 0):
+		for i in range(args.size()):
+			super_params += args[i].p_name
+			if(i != args.size() -1):
+				super_params += ', '
+
+	text = _init_template.format({
+		"func_decleration":decleration,
+		"super_params":super_params,
+		"param_array":param_array,
+		"method_name":meta.name,
+	})
+
+	return text
+
 # ###############
 # Public
 # ###############
+
+
 
 # Creates a delceration for a function based off of function metadata.  All
 # types whose defaults are supported will have their values.  If a datatype
 # is not supported and the parameter has a default, a warning message will be
 # printed and the declaration will return null.
-func get_function_text(meta, path=null, override_size=null):
-	var method_params = ''
+func get_function_text(parsed_method, singleton=null):
+	var meta = parsed_method.meta
 	var text = null
-	var result = _make_arg_array(meta, override_size)
-	var has_unsupported = result[0]
-	var args = result[1]
-
+	var args = _make_arg_array(meta)
 	var param_array = _get_spy_call_parameters_text(args)
-
-	if(has_unsupported):
-		# This will cause a runtime error.  This is the most convenient way to
-		# to stop running before the error gets more obscure.  _make_arg_array
-		# generates a gut error when unsupported defaults are found.
-		method_params = null
-	else:
-		method_params = _get_arg_text(args);
+	var method_params = _get_arg_text(args);
 
 	if(param_array == 'null'):
 		param_array = '[]'
 
 	if(method_params != null):
-		var decleration = str('func ', meta.name, '(', method_params, '):')
-		text = _func_text.format({
-			"func_decleration":decleration,
-			"method_name":meta.name,
-			"param_array":param_array,
-			"super_call":_get_super_call_text(meta.name, args)
-		})
+		if(meta.name == '_init'):
+			text =  _get_init_text(meta, args, method_params, param_array)
+		else:
+			var return_it = ''
+			if(parsed_method.return_type_text != 'void'):
+				return_it = 'return '
+
+			var decleration = str('func ', meta.name, '(', method_params, '):')
+			text = _func_template.format({
+				"func_decleration": decleration,
+				"method_name": meta.name,
+				"param_array": param_array,
+				"super_call": _get_super_call_text(parsed_method, singleton),
+				"return_it": return_it
+			})
+
 	return text
 
 
